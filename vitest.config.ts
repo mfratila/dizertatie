ac37f5fc5c469ff42f387621862028064d927f57
MihaiFratila <mihai.fratila01@gmail.com>
2026-02-17 02:56:22 +0200

    feat: add path alias resolution to vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import path from 'path/win32';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'node',
